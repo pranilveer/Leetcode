72cb098f50367786af45f799cab3f920f182694e
pranilveer <55059445+pranilveer@users.noreply.github.com>
2026-07-25 23:57:30 +0530

    Time: 0 ms (100%), Space: 8.7 MB (74.03%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        int temp = n;
-        int ans = 1;
-        vector<int> v1;
-        while(temp>0){
-            int ld = temp%10;
-            v1.push_back(ld);
-            temp/=10;
-        }
-        sort(v1.begin(), v1.end());
-        int n1= v1.size()-1;
+        int first =0, second =0;
 
-        return max((v1[0]*v1[1]), (v1[n1-1]*v1[n1]));
+        while(n>0){
+            int ld = n%10;
+            if(ld>=first){
+                second = first;
+                first = ld;
+            }else if(ld>second){
+                second=ld;
+            }
+            n/=10;
+        }
+        return first*second;
     }
 };
