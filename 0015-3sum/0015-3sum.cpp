bc404e22754bf7745d0ce06f1041a083b95b0a5b
pranilveer <55059445+pranilveer@users.noreply.github.com>
2026-07-28 23:59:33 +0530

    Time: 50 ms (46.56%), Space: 28.9 MB (99.35%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,35 +4,35 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
 
-        for(int i = 0; i < nums.size() - 2; i++){
-            if(i > 0 && nums[i] == nums[i-1])
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            int left = i + 1;
-            int right = nums.size() - 1;
+            int left = i + 1, right = nums.size() - 1;
 
-            while(left < right){
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0){
+
+                if (sum == 0) {
                     ans.push_back({nums[i], nums[left], nums[right]});
                     left++;
                     right--;
-                    
-                    while(left < right && nums[left] == nums[left-1])
-                        left++;
 
-                    while(left < right && nums[right] == nums[right+1])
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                        continue;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
-                }
-                else if(sum < 0){
-                    left++;
-                }
-                else{
+                        continue;
+                    }
+                } else if (sum > 0) {
                     right--;
+                } else {
+                    left++;
                 }
             }
         }
-
         return ans;
     }
 };
